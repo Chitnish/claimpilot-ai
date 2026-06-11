385a73f61791aed7c20787ef2a7769dd4187f353
Chitnish <anishchitnis8@gmail.com>
2026-06-11 09:57:35 -0400

    updated start scripts

--- start-backend.ps1
+++ start-backend.ps1
@@ -1,4 +1,4 @@
-﻿$env:PATH = "$env:PATH;C:\Program Files\nodejs;C:\poppler\poppler-24.08.0\Library\bin"
+﻿$env:PATH = "$env:PATH;C:\Program Files\nodejs;C:\Program Files\Git\cmd;C:\poppler\poppler-24.08.0\Library\bin"
 Set-Location "C:\Users\AnishChitnis(Intern)\projects\claimpilot-ai\backend"
 .\.venv\Scripts\Activate.ps1
 uvicorn app.main:app --reload --port 8000
--- start-frontend.ps1
+++ start-frontend.ps1
@@ -1,3 +1,3 @@
-﻿$env:PATH = "$env:PATH;C:\Program Files\nodejs"
+﻿$env:PATH = "$env:PATH;C:\Program Files\nodejs;C:\Program Files\Git\cmd;C:\poppler\poppler-24.08.0\Library\bin"
 Set-Location "C:\Users\AnishChitnis(Intern)\projects\claimpilot-ai\frontend"
 npm run dev
